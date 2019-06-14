814d48aa657dd701568cfd8a556290348994b393
Alexey Mayants <a.mayants@cityads.ru>
2019-06-14 13:34:17 +0300

    Add calendar interactivity, date and time selection

--- work/cityads/special/shusurf/js/main.js
+++ work/cityads/special/shusurf/js/main.js
@@ -235,6 +235,183 @@
     });
   }
 
+  function getMonthData(callback) {
+    setTimeout(function () {
+      callback({
+        dates: [
+          "2019-05-27",
+          "2019-05-28",
+          "2019-05-29",
+          "2019-05-30",
+          "2019-05-31",
+          "2019-06-01",
+          "2019-06-02",
+          "2019-06-03",
+          "2019-06-04",
+          "2019-06-05",
+          "2019-06-06",
+          "2019-06-07",
+          "2019-06-08",
+          "2019-06-09"
+        ],
+        unavailable: [
+          "2019-05-28",
+          "2019-06-01"
+        ]
+      });
+    }, 500);
+  }
+
+  var MONTHS = ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'];
+  var WEEKEND_DAYS = [6,0];
+  var WEEK_LENGTH = 7;
+
+  function displayMonthData(monthData) {
+    var firstDate = new Date(monthData.dates[0]),
+        lastDate = new Date(monthData.dates[monthData.dates.length - 1]);
+
+    $('.sh-book__month-header').text(firstDate.getMonth() === lastDate.getMonth() ?
+      MONTHS[firstDate.getMonth()] + ' ' + firstDate.getFullYear() :
+      MONTHS[firstDate.getMonth()] + '-' + MONTHS[lastDate.getMonth()].toLowerCase() + ' ' + firstDate.getFullYear()
+    );
+
+    var dateChunks = [];
+
+    for (var i = 0; i < monthData.dates.length; i += WEEK_LENGTH) {
+      dateChunks.push(monthData.dates.slice(i, i + WEEK_LENGTH));
+    }
+
+    var html = dateChunks.map(function (dates) {
+      return '<tr>' +
+        dates.map(function (date) {
+          var d = new Date(date);
+
+          return '<td' +
+            ' data-date="' + date + '"' +
+            ' class="sh-calendar__cell ' +
+              (monthData.unavailable.indexOf(date) >= 0 ? ' sh-calendar__cell--unavailable' : '') +
+              (WEEKEND_DAYS.indexOf(d.getDay()) >= 0 ? ' sh-calendar__cell--weekend' : '') +
+            '"' +
+          '>28</td>';
+        }).join('')
+      '</tr>';
+    });
+
+    $('.sh-book__month-table tbody').html(html);
+    $('.sh-book__month').show();
+  }
+
+  function getDayData(date, callback) {
+    setTimeout(function () {
+      callback([
+        {
+          name: 'Утро',
+          price: 'Стандарт: 4500 ₽, Абонемент: 4000 ₽',
+          dates: ["9:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30", "21:00", "21:30", "22:00", "22:30"],
+          unavailable: ["22:00"]
+        },
+        {
+          name: 'Вечер',
+          price: 'Стандарт: 5500 ₽, Абонемент: 5000 ₽',
+          isNight: true,
+          dates: ["23:30", "00:00", "00:30", "01:00", "01:30", "02:00"],
+          unavailable: []
+        }
+      ]);
+    }, 500);
+  }
+
+  var HOUR_ROW_WIDTH = 6;
+  var MONTHS_OF_YEAR = ['января','Февраля','Марта','апреля','мая','июня','июля','августа','сентября','октября','ноября','декабря'];
+  var DAYS_OF_WEEK = ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'];
+
+  function displayDayData(date, dayData) {
+    var html = dayData.map(function (dayPart) {
+          var dateChunks = [];
+
+          for (var i = 0; i < dayPart.dates.length; i += HOUR_ROW_WIDTH) {
+            dateChunks.push(dayPart.dates.slice(i, i + HOUR_ROW_WIDTH));
+          }
+
+          return '<tbody>' +
+            '<tr><td colspan="6" class="sh-calendar__daytime"><div class="ca-daytime-header">' +
+              '<span class="ca-daytime-header__name">' + dayPart.name + '</span>' +
+              '<span class="ca-daytime-header__price">' + dayPart.price + '</span>' +
+            '</div></td></tr>' +
+            dateChunks.map(function (hours) {
+              var hoursRow = hours.map(function (hour) {
+                    return '<td' +
+                      ' data-time="' + hour + '"' +
+                      ' class="sh-calendar__cell' +
+                        (dayPart.isNight ? ' sh-calendar__cell--night' : '') +
+                        (dayPart.unavailable.indexOf(hour) >= 0 ? ' sh-calendar__cell--unavailable' : '') +
+                      '"' +
+                    '>' + hour + '</td>';
+                  }).join('');
+
+              for (var i = hours.length; i < HOUR_ROW_WIDTH; i++) {
+                hoursRow += '<td class="sh-calendar__cell' + (dayPart.isNight ? ' sh-calendar__cell--night' : '') + '"></td>';
+              }
+
+              return '<tr>' +
+                hoursRow +
+              '</tr>';
+            }).join('') +
+          '</tbody>';
+        });
+
+    var d = new Date(date);
+
+    $('.sh-book__day-header').text(d.getDate() + ' ' + MONTHS_OF_YEAR[d.getMonth()] + ', ' + DAYS_OF_WEEK[d.getDay()]);
+    $('.sh-book__day-table').html(html);
+    $('.sh-book__day').show();
+  }
+
+  function initCalendar() {
+    getMonthData(function (monthData) {
+      displayMonthData(monthData);
+    });
+
+    $('.sh-book__month-table').on('click', '.sh-calendar__cell', function () {
+      var $this = $(this),
+          date = $this.data('date');
+
+      if ($this.hasClass('sh-calendar__cell--unavailable') || $this.is(':empty')) {
+        return;
+      }
+
+      $('.sh-book__month-table').find('.sh-calendar__cell').removeClass('sh-calendar__cell--selected sh-calendar__cell--current');
+      $this.addClass('sh-calendar__cell--selected sh-calendar__cell--current sh-calendar__cell--in-progress');
+
+      getDayData(date, function (dayData) {
+        $this.removeClass('sh-calendar__cell--in-progress');
+
+        displayDayData(date, dayData);
+      });
+    });
+
+    $('.sh-book__day-table').on('click', '.sh-calendar__cell', function () {
+      var $this = $(this),
+          date = $this.data('date');
+
+      if ($this.hasClass('sh-calendar__cell--unavailable') || $this.is(':empty')) {
+        return;
+      }
+
+      if ($this.hasClass('sh-calendar__cell--selected')) {
+        $this.removeClass('sh-calendar__cell--selected sh-calendar__cell--current');
+        return;
+      }
+
+      if ($('.sh-book__day-table .sh-calendar__cell--selected').length >= 4) {
+        return;
+      }
+
+      $('.sh-book__day-table .sh-calendar__cell--current').removeClass('sh-calendar__cell--current');
+      $this.addClass('sh-calendar__cell--selected sh-calendar__cell--current');
+    });
+  }
+
   function loginViaSocnet(code, callback) {
     // TODO
     setTimeout(function () {
@@ -264,6 +441,7 @@
     highlightFeature: highlightFeature,
     initSliders: initSliders,
     initSocial: initSocial,
+    initCalendar: initCalendar,
     initForm: initForm
   };
 })(document, window);
